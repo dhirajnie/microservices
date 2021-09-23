f895911f2fa265a92e52cdcf9024000dd3c16078
dhiraj kumar <dhirajnie@gmail.com>
2021-09-23 13:44:16 +0530

    okta ui integration

---
+++ 26-checkout-form-save-the-order-frontend/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+
+    oidc: {
+        clientId: '0oa1ynbxs90mc3lJD5d7',
+        issuer: 'https://dev-31445677.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+        // dev-31445677.okta.com÷
+    }
+
+}
