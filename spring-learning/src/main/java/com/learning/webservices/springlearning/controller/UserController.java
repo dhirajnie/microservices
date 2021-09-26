b6baa9921626dfbe8a817f6270cfbb885b6710ab
dhiraj kumar <dhirajnie@gmail.com>
2021-09-26 13:07:57 +0530

    intial commit

---
+++ spring-learning/src/main/java/com/learning/webservices/springlearning/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.learning.webservices.springlearning.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@Slf4j
+@RestController
+@RequestMapping("users")
+public class UserController {
+    @GetMapping(path = "/{userId}")
+    String getuser(@PathVariable String userId){
+        log.info("Request recived for user "+userId);
+
+        return "API working "+userId;
+    }
+}
